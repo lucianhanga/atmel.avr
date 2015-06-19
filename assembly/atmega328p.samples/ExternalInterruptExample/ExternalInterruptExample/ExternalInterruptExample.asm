cb575087705eb361a1988c7a029068caf28fe4b8
Lucian H <lucianhanga@googlemail.com>
2015-06-19 10:02:36 +0200

    - add to git a text document which contains the pinout of the atmega328p - add to git the example for the External Interrupt

---
+++ assembly/atmega328p.samples/ExternalInterruptExample/ExternalInterruptExample/ExternalInterruptExample.asm
@@ -0,0 +1,23 @@
+;--------10--------20--------30--------40--------50--------60--------70--------80--------90------100
+;
+;       This example will show/explain step by step how to programm the ATMega328p to accept
+;       external interrupts. In this sample the INT0 will bused.
+;
+; atmega328p pinout
+;                                      _______  _______
+;                  Reset-PCINT14-PC6 -| 1            28|- PC5-PCINT13-ADC5-A5|A5-SCL
+;                 0--RXD-PCINT16-PD0 -| 2            27|- PC4-PCINT12-ADC4-A4|A5-SDA 
+;                 1--TXD-PCINT17-PD1 -| 3            26|- PC3-PCINT11-ADC3-A3|A5
+;                 2-INT0-PCINT18-PD2 -| 4            25|- PC2-PCINT10-ADC2-A2|A5
+;        OC2B-PWM-3-INT1-PCINT19-PD3 -| 5            24|- PC1-PCINT9--ADC1-A1|A5
+;             T0--4-XCK--PCINT20-PD4 -| 6            23|- PC0-PCINT8--ADC0-A0|A5
+;                                VCC -| 7            22|- GND
+;                                GND -| 8            21|- AREF
+;             OSC1-XTAL1-PCINT6--PB6 -| 9            20|- VCC
+;             OSC2-XTAL2-PCINT7--PB7 -|10            19|- PB5-PCINT5------13-----SCK
+;        OC0B-PWM-5--T1--PCINT21-PD5 -|11            18|- PB4-PCINT4------12-----MISO
+;        OC0A-PWM-6-AIN0-PCINT22-PD6 -|12            17|- PB3-PCINT3-OC2A-11-PWM-MOSI
+;                 7-AIN1-PCINT23-PD7 -|13            16|- PB2-PCINT2-OC1B-10-PWM-SS
+;            ICP1-8-CLK0-PCINT0--PB0 -|14            15|- PB1-PCINT1-OC1A--9-PWM
+;                                     ------------------
+;
