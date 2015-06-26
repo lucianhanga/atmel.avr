f6c546e7cb200638e5e77b248c3cbcff4179a745
Lucian H <lucianhanga@googlemail.com>
2015-06-26 16:41:25 +0200

    continue the development and add some more information about PWM

--- assembly/atmega328p.samples/PWMwithTimer0Example/PWMwithTimer0Example/PWMwithTimer0Example.asm
+++ assembly/atmega328p.samples/PWMwithTimer0Example/PWMwithTimer0Example/PWMwithTimer0Example.asm
@@ -1,7 +1,22 @@
 ;--------10--------20--------30--------40--------50--------60--------70--------80--------90------100
 ;
 ;   The example will program the Counter/Timer0 to generate PWM (Pulse Width Modulator)
-;   with a configurable PWM Periods. The most important registers to remember for this programm:
+;   with a configurable PWM Periods. 
+;   PWM defintions: 
+;       Duty Cycle = 100% x Pulse Width/Period
+;       Where:
+;                Duty Cycle in (%)
+;       Pulse Width = Time the signal is in the ON or high state (sec) 
+;       Period = Time of one cycle (sec)
+;     
+;       ON    ______          ________          _________
+;                   |        | PulseW.|        |
+;                   |        |<------>|        |
+;       OFF         |________|        |________|
+;                             <--------------->
+;                                   Period
+;
+;   The most important registers to remember for this programm:
 ;       PRR - Power Reduction Register (anble/disable the timer) - PIRTIM0 bit should be reseted
 ;       TIMSK0 - Timer/counter0 interrupt MaSK register
 ;       TCCR0A - Timer/Counter0 Control Register A
@@ -43,31 +58,78 @@
                 ; define the interrupt table
                 .org    0               jmp     start   ; RESET interrupt routine
                 .org    OVF0addr        jmp     OVF0vec ; OVerFlow Counter/Timer0 interrupt routine
+                .org    OC0Aaddr        jmp     OC0Avec ; Output Compare A Counter/Timer0 interrupt
 
                 .org            INT_VECTORS_SIZE        ; end of the interrupt vectors table
 start:          ; program start and the RESET interrupt vector 
                 cli
                 initStk                                 ; initialize stack for calls and iterrupts
+                sbi             DDRD, DDD6              ; enavle PD6 for output
                 ; enable the Timer/Counter0
                 lds             r20, PRR                ; Power Reduction Register (!MemoryMapped!)
                 andi            r20, ~(1 << PRTIM0)     ; enable the Timer/Counter0
                 sts             PRR, r20                ;
-                ; setup the clk
-                ;  CS02  CS01  CS00
-                ;    0     0     0      No clock source (Timer/Counter stopped)
-                ;    0     0     1      clkI/O/(No prescaling)
-                ;    0     1     0      clkI/O/8 (From prescaler)
-                ;    0     1     1      clkI/O/64 (From prescaler)
-                ;    1     0     0      clkI/O/256 (From prescaler)
-                ;    1     0     1      clkI/O/1024 (From prescaler)
-                ;    1     1     0      External clock source on T0 pin. Clock on falling edge.
-                ;    1     1     1      External clock source on T0 pin. Clock on rising edge.
-                lds             r20, 1<<CS02 | 1<<CS00  ; select clock clkI/0 1024 prescaling 
-                out             TCCR0B, r20             ; 
-                
-                
+                ; 
+                ; clock 
+                ; CS02  CS01  CS00
+                ;   0     0     0      No clock source (Timer/Counter stopped)
+                ;   0     0     1      clkI/O/(No prescaling)
+                ;   0     1     0      clkI/O/8 (From prescaler)
+                ;   0     1     1      clkI/O/64 (From prescaler)
+                ;   1     0     0      clkI/O/256 (From prescaler)
+                ;   1     0     1      clkI/O/1024 (From prescaler)
+                ;   1     1     0      External clock source on T0 pin. Clock on falling edge.
+                ;   1     1     1      External clock source on T0 pin. Clock on rising edge.
+                ; 
+                ; mode
+                ; Mode  WGM02 WGM01 WGM00  Name               TOP     Update OCRx    TOV
+                ;   
+                ;  0      0     0     0    Normal             0xFF    immediate      MAX
+                ;  1      0     0     1    PWM,Phase Correct  0xFF    TOP            BOTTOM 
+                ;  2      0     1     0    CTC                OCRA    imediate       MAX
+                ;  3      0     1     1    Fast PWM           0xFF    BOTTOM         MAX
+                ;  4      1     0     0    -------            ----    -------        ----
+                ;  5      1     0     1    PWM,Phase Correct  OCRA    TOP            BOTTOM
+                ;  6      1     1     0    -------            ----    -------        ----
+                ;  7      1     1     1    Fast PWM           OCRA    BOTTOM         TOP
+                ; 
+                ;  MAX = 0xFF, BOTTOM = 0xFF
+                ; 
+                ; OC0A - Output Compare Pin A for Timer/Counter 0 (for WGM 0&2)
+                ; COM0A1 COM0A0  Description
+                ;
+                ;   0      0     Normal port operation, OC0A disconected
+                ;   0      1     Toggle OC0A on Compare match
+                ;   1      0     Clear OC0A on Compare match
+                ;   1      1     Set OC0A on Compare match
+                ; 
+                ; OC0A - Output Compare Pin A for Timer/Counter 0 (for WGM 3&7)
+                ; COM0A1 COM0A0  Description
+                ;
+                ;   0      0     Normal port operation, OC0A disconnected
+                ;   0      1     Toggle OC0A on Compare Match. (WGM02 <- 1)
+                ;   1      0     Clear OC0A on Compare Match, set OC0A at BOTTOM (non-inverting)
+                ;   1      1     Set OC0A on Compare Match, clear OC0A at BOTTOM (inverting mode)
+                ; 
+                ;  CS02:0    <---  0 0 1 (clkI/O no prescaling)
+                ;  WGM02:0   <---  1 1 1 (Fast PWM mode)
+                ;  COM0A1:0  <---  1 0   (clear OC0A on compare match, set at BOTTOM)
+                ;
+                ldi             r20, 1 << CS00 | 1 << WGM02
+                out             TCCR0B, r20                         
+                ldi             r20, 1 << COM0A1 | 1 << WGM01 | 1 << WGM00 
+                out             TCCR0A, r20                
+                ; enable the interrupt(s)
+                ldi             r20, 1 << TOIE0 | 1 << OCIE0A
+                sts             TIMSK0, r20             ;
+                ldi             r20, 128                ; puls width <- 128 ( duty cycle = 50%)
+                out             OCR0A, r20              ; 
+                sei                                     ; enable interrupts
 end:            rjmp            end                     ; infinite loop at the end of the programm
 
 
 OVF0vec:        ; interrupt vector for the OVF0 interrupt
+                reti
+
+OC0Avec:        ; interrupt vector for the OC0A interrupt
                 reti
